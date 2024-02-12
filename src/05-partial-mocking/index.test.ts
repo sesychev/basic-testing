2f4016a93fccec4ca0cf8aa3cc7b8958036c79a4
sesychev <sergeysychou@gmail.com>
2024-02-12 13:58:06 +0300

    feat: 05-partial-mocking

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,20 +1,38 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  return {
+    __esModule: true,
+    ...originalModule,
+    unmockedFunction: 'I am not mocked',
+    mockOne: jest.fn(),
+    mockTwo: jest.fn(),
+    mockThree: jest.fn(),
+  };
 });
 
 describe('partial mocking', () => {
+  beforeEach(() => jest.spyOn(console, 'log').mockImplementation());
+
+  afterEach(() => jest.clearAllMocks());
+
   afterAll(() => {
     jest.unmock('./index');
   });
 
-  test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+  test('mockOne, mockTwo, mockThree should not log into console', async () => {
+    mockOne();
+    mockTwo()
+    mockThree()
+    expect(mockOne).toHaveBeenCalled();
+    expect(mockTwo).toHaveBeenCalled();
+    expect(mockThree).toHaveBeenCalled();
+    expect(console.log).not.toHaveBeenCalled();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    expect(unmockedFunction).toBe('I am not mocked');
   });
 });
